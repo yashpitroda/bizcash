5c175a6070e7b48a77dc7af49e9677f3e53c6e14
yashpitroda <yashpitroda06@gmail.com>
2024-05-22 09:16:34 +0530

    firebase config

---
+++ lib/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  final String title;
+  const HomeScreen({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Container(
+        child: Text(title),
+      ),
+    );
+  }
+}
